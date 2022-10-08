30f39307743aeddd8d23abf6c7c762172ef6fcfd
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-10-08 11:24:36 -0400

    added templates and routs

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -5,11 +5,15 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello():
+    def index():
         return 'Hello, PetFax!'
     
     # register pet blueprint 
     from . import pet
+    app.register_blueprint(pet.bp)
+
+    #register fact blueprint
+    from . import fact
     app.register_blueprint(pet.bp)
 
     # return the app 
---
+++ petfax/fact.py
@@ -0,0 +1,8 @@
+import re
+from flask import ( Blueprint, render_template)
+
+bp = Blueprint('fact', __name__, url_prefix='/facts')
+
+@bp.route('/new')
+def new():
+    return render_template('facts/new.html')
